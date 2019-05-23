c1c4b0a51798a8557be8d1818a3b0b59640eb9b2
serik_lav <serik_lav@mail.ru>
2019-05-24 00:20:15 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.sevenpowerx.loc/admin/');
+define('HTTP_CATALOG', 'http://opencart.sevenpowerx.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.sevenpowerx.loc/admin/');
+define('HTTPS_CATALOG', 'http://opencart.sevenpowerx.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/admin/');
+define('DIR_SYSTEM', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/');
+define('DIR_IMAGE', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/image/');
+define('DIR_LANGUAGE', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/admin/language/');
+define('DIR_TEMPLATE', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/admin/view/template/');
+define('DIR_CONFIG', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/config/');
+define('DIR_CACHE', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/download/');
+define('DIR_LOGS', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/upload/');
+define('DIR_CATALOG', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart_sevenpowerx');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.sevenpowerx.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.sevenpowerx.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/catalog/');
+define('DIR_SYSTEM', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/');
+define('DIR_IMAGE', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/image/');
+define('DIR_LANGUAGE', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/catalog/language/');
+define('DIR_TEMPLATE', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/config/');
+define('DIR_CACHE', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/download/');
+define('DIR_LOGS', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/serik_lav/Documents/sevenpowerx/opencart.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart_sevenpowerx');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
